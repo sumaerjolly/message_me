606efd9d20e8056440b71b201bf1c672450854ce
Sumaer Jolly <sumaerjolly@gmail.com>
2019-09-09 22:43:55 +0530

    improve display

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -2,7 +2,7 @@ class ChatroomController < ApplicationController
   before_action :require_user
 
   def index
-    @messages = Message.all 
     @message = Message.new
+    @messages = Message.custom_display
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,5 @@
 class Message < ApplicationRecord
   belongs_to :user
   validates :body, presence: true, length: { minimum:3 }
+  scope :custom_display, -> { order(:created_at).last(20) }
 end
