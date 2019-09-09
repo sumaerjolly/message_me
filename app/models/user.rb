d3f7e6a3d5ce190b329c641a66947cc89a681c4f
Sumaer Jolly <sumaerjolly@gmail.com>
2019-09-09 15:51:25 +0530

    Add message model and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
     validates :username, presence: true, length: { in: 3..20 },  uniqueness: { case_sensitive: false }
+    has_many :messages
     has_secure_password
 end
